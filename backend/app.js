60ce4ba0a9d75c257d12ee970b956d6a177f8b1f
CarlosFedericoZalazar <carlos-zalazar@hotmail.com>
2026-03-16 23:16:45 -0300

    configuro cors

--- backend/app.js
+++ backend/app.js
@@ -11,7 +11,14 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(cors());
+app.use(cors({
+  origin: [
+    "http://localhost:5500",
+    "https://identity-frontend-flax.vercel.app"
+  ],
+  methods: ["GET","POST","PUT","DELETE"],
+  credentials: true
+}));
 
 app.use('/api/auth', authRoutes);
 
