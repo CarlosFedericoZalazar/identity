834edd1ce7d86ecbd9c05fd67df0b69ee83a4f12
CarlosFedericoZalazar <carlos-zalazar@hotmail.com>
2026-03-17 00:16:54 -0300

    correccion en superadmin.middleware.js

--- backend/routes/superadmin.routes.js
+++ backend/routes/superadmin.routes.js
@@ -1,10 +1,10 @@
 import express from "express";
 import { authMiddleware } from "../middlewares/authMiddleware.js";
-import { superAdminMiddleware } from "../middlewares/superAdmin.middleware.js";
+import { superAdminMiddleware } from "../middlewares/superadmin.middleware.js";
 import { deleteUser } from "../controllers/superAdmin.controller.js";
 
 const router = express.Router();
 
-router.delete("/delete-user/:id",authMiddleware,superAdminMiddleware,deleteUser);
+router.delete("/delete-user/:id",authMiddleware, superAdminMiddleware, deleteUser);
 
 export default router;
